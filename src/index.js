230a8fde35ccb96770751fdcadb59e51f2997bb9
Maksim G. <golotmaks@gmail.com>
2026-05-07 10:44:41 +0300

    feat: function check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
-module.exports = function check(/* str, bracketsConfig */) {
-  throw new Error('Not implemented');
+module.exports = function check(str, bracketsConfig) {
+  const stack = [];
+  const openBrackets = [];
+  const bracketsMap = {};
+  bracketsConfig.forEach(([open, close]) => {
+    openBrackets.push(open);
+    bracketsMap[close] = open;
+  });
+  for (let char of str) {
+    const lastInStack = stack[stack.length - 1];
+    if (bracketsMap[char] !== undefined) {
+      if (char === bracketsMap[char]) {
+        if (lastInStack === char) {
+          stack.pop();
+        } else {
+          stack.push(char);
+        }
+      } else {
+        if (stack.length === 0 || stack.pop() !== bracketsMap[char]) {
+          return false;
+        }
+      }
+    } else {
+      stack.push(char);
+    }
+  }
+  return stack.length === 0;
 };
